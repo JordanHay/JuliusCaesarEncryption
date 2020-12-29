00fcd7872577fc06c40c13e738210bf7e4801000
JordanHay <51302288+JordanHay@users.noreply.github.com>
2020-12-29 10:12:01 -0600

    Add files via upload

---
+++ JuliusCaesar/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace JuliusCaesar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "experience is the teacher of all things";
+
+            int key = 3;
+
+            string encrypted = Encrypt(message, key);
+            Console.WriteLine(encrypted);
+
+            Console.ReadKey();
+
+            
+        }
+
+        public static string Encrypt(string message, int key)
+        {
+            message = message.ToUpper();
+
+            string encryptedMessage = "";
+
+            for(int index = 0; index < message.Length; index++)
+            {
+                char encryptedLetter = Encrypt(message[index], key);
+                encryptedMessage += encryptedLetter;
+            }
+            return encryptedMessage;
+        }
+
+        private static char Encrypt(char letter, int key)
+        {
+            if(letter < 'A' || letter > 'Z') { return letter; }
+
+            int letterAsNumber = (int)letter - 'A';
+
+            int encryptedLetter = (letterAsNumber + key) % 26;
+
+            return (char)(encryptedLetter + 'A');
+        }
+    }
+}
